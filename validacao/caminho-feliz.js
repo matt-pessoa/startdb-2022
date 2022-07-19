575f7822cd6f5868e3aa127c77104a96413961f8
Matheus Pessoa <matheuspessoafonseca@gmail.com>
2022-07-19 14:28:43 -0300

    clean

--- validacao/caminho-feliz.js
+++ validacao/caminho-feliz.js
@@ -12,9 +12,7 @@ let estadosEstaoCorretos = validarEtapa(
   jogoForca
 );
 
-console.log(jogoForca.vidas);
-['a', 'b', 'c', 'x', 'i'].forEach((letra) => jogoForca.chutar(letra)); // ao rodar o comando de chutes, as vidas são contadas, portanto não é possível que a validação abaixo encontre as 6 vidas inicias (o programa não foi resetado), mas sim a 1 vida restante
-console.log(jogoForca.vidas);
+['a', 'b', 'c', 'x', 'i'].forEach((letra) => jogoForca.chutar(letra));
 
 estadosEstaoCorretos =
   estadosEstaoCorretos &&
--- validacao/validar-etapa.js
+++ validacao/validar-etapa.js
@@ -14,26 +14,6 @@ const validarEtapa = (
   const letrasChutadas = arrLetrasChutadas.join('');
   const estado = jogoForca.buscarEstado();
 
-  console.log('VIDAS');
-  console.log(vidas, vidasEsperadas);
-  console.log(vidas === vidasEsperadas);
-  console.log('\n----------next----------\n');
-
-  console.log('PALAVRA');
-  console.log(palavra, palavraEsperada);
-  console.log(palavra === palavraEsperada);
-  console.log('\n----------next----------\n');
-
-  console.log('LETRAS');
-  console.log(letrasChutadas, letrasChutadasEsperadas);
-  console.log(letrasChutadas === letrasChutadasEsperadas);
-  console.log('\n----------next----------\n');
-
-  console.log('ESTADO');
-  console.log(estado, estadoEsperado);
-  console.log(estado === estadoEsperado);
-  console.log('\n----------next----------\n');
-
   return (
     vidas === vidasEsperadas &&
     palavra === palavraEsperada &&
